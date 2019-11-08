d46a7c64e19c9abbe8818572049dfb3020772d27
Islam Kaloop <islamkaloop@gmail.com>
2019-11-08 14:36:44 +0200

    draw cube and ball

--- Bouncing Ball/main.cpp
+++ Bouncing Ball/main.cpp
@@ -10,29 +10,92 @@ void Display(void);
 int window_height = 600;
 int window_width = 1000;
 
+void initGL() {
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 
+	glClearDepth(1.0f);                  
+	glEnable(GL_DEPTH_TEST);   
+	glDepthFunc(GL_LEQUAL);    
+	glShadeModel(GL_SMOOTH);  
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  
+}
+
+void reshape(GLsizei width, GLsizei height) {  
+	if (height == 0) height = 1;                
+	GLfloat aspect = (GLfloat)width / (GLfloat)height;
+
+	glViewport(0, 0, width, height);
+
+	glMatrixMode(GL_PROJECTION);  
+	glLoadIdentity();             
+	gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+}
+
 int main(int argc, char** argr)
 {
-	glutInit(&argc, argr);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(1000, window_height);
-	glutInitWindowPosition(250, 50);
-	glutCreateWindow("Bouncing Ball");
-	glutDisplayFunc(Display);
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	gluOrtho2D(0.0, window_width, 0.0, window_height);
-
-	glutMainLoop();
+	glutInit(&argc, argr);          
+	glutInitDisplayMode(GLUT_DOUBLE); 
+	glutInitWindowSize( window_width, window_height);
+	glutInitWindowPosition(50, 50); 
+	glutCreateWindow("Bouncing Ball");         
+	glutDisplayFunc(Display);       
+	glutReshapeFunc(reshape);       
+	initGL();                      
+	glutMainLoop();                 
 	return 0;
 }
 
 void Display(void)
 {
-	glColor3f(1,1,1);
-	glBegin(GL_QUADS);
-		glVertex2f(0, 0);
-		glVertex2f(0, 50);
-		glVertex2f(50,50);
-		glVertex2f(50, 0);
-	glEnd();
-	glFlush();
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);    
+
+	glLoadIdentity();                
+	glTranslatef(1.5f, 0.0f, -7.0f);
+	glRotatef(45.0f, 1.0f, 1.0f, 1.0f);
+
+	glBegin(GL_QUADS);                
+	glColor3f(0.0f, 1.0f, 0.0f);     
+	glVertex3f(1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, 1.0f);
+	glVertex3f(1.0f, 1.0f, 1.0f);
+
+	glColor3f(1.0f, 0.5f, 0.0f);    
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+
+	glColor3f(1.0f, 0.0f, 0.0f);   
+	glVertex3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(-1.0f, 1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
+
+	glColor3f(1.0f, 1.0f, 0.0f);     
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(1.0f, 1.0f, -1.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);    
+	glVertex3f(-1.0f, 1.0f, 1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+
+	glColor3f(1.0f, 0.0f, 1.0f);     
+	glVertex3f(1.0f, 1.0f, -1.0f);
+	glVertex3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glEnd();  
+
+	glColor3f(1.0, 0.0, 0.0);
+	glLoadIdentity();
+	glTranslatef(-1.5f, 0.0f, -7.0f);
+	glRotatef(45.0f, 1.0f, 0.0f, 0.0f);
+	glutWireSphere(1.0, 20.0, 20.0);
+
+	glutSwapBuffers();
 }
