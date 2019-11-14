cf91edd099fbf343d234a0de356d7a234980d581
Islam Kaloop <islamkaloop@gmail.com>
2019-11-14 23:51:35 +0200

    add some features

--- Bouncing Ball/main.cpp
+++ Bouncing Ball/main.cpp
@@ -7,108 +7,216 @@ using namespace std;
 
 void Display(void);
 void Anim(void);
+void key(int key, int x, int y);
+void spaceKey(unsigned char key, int x, int y);
+void spaceKeyUp(unsigned char key, int x, int y);
 
 int window_height = 600;
 int window_width = 1000;
-GLfloat  rotateCube = 0.0f;
-GLfloat  rotateBall = 0.0f;
+float rotAng;
+float camera=0;
+float add = 0.01;
+float PointerX = 0;
+float PointerY = 5;
+float Power = 0;
+int shoot = 0;
+float ballPosX = 0;
+float ballPosY = 5;
+float ballPosZ = 0;
+float moveByPower = 0;
+#define RADPERDEG 0.0174533
+void Arrow(GLdouble x1, GLdouble y1, GLdouble z1, GLdouble x2, GLdouble y2, GLdouble z2, GLdouble D)
+{
+	double x = x2 - x1;
+	double y = y2 - y1;
+	double z = z2 - z1;
+	double L = sqrt(x * x + y * y + z * z);
 
-void initGL() {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 
-	glClearDepth(1.0f);                  
-	glEnable(GL_DEPTH_TEST);   
-	glDepthFunc(GL_LEQUAL);    
-	glShadeModel(GL_SMOOTH);  
-	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  
+	GLUquadricObj* quadObj;
+
+	glPushMatrix();
+
+	glTranslated(x1, y1, z1);
+
+	if ((x != 0.) || (y != 0.)) {
+		glRotated(atan2(y, x) / RADPERDEG, 0., 0., 1.);
+		glRotated(atan2(sqrt(x * x + y * y), z) / RADPERDEG, 0., 1., 0.);
+	}
+	else if (z < 0) {
+		glRotated(180, 1., 0., 0.);
+	}
+
+	glTranslatef(0, 0, L - 4 * D);
+
+	quadObj = gluNewQuadric();
+	gluQuadricDrawStyle(quadObj, GLU_FILL);
+	gluQuadricNormals(quadObj, GLU_SMOOTH);
+	gluCylinder(quadObj, 2 * D, 0.0, 4 * D, 32, 1);
+	gluDeleteQuadric(quadObj);
+
+	quadObj = gluNewQuadric();
+	gluQuadricDrawStyle(quadObj, GLU_FILL);
+	gluQuadricNormals(quadObj, GLU_SMOOTH);
+	gluDisk(quadObj, 0.0, 2 * D, 32, 1);
+	gluDeleteQuadric(quadObj);
+
+	glTranslatef(0, 0, -L + 4 * D);
+
+	quadObj = gluNewQuadric();
+	gluQuadricDrawStyle(quadObj, GLU_FILL);
+	gluQuadricNormals(quadObj, GLU_SMOOTH);
+	gluCylinder(quadObj, D, D, L - 4 * D, 32, 1);
+	gluDeleteQuadric(quadObj);
+
+	quadObj = gluNewQuadric();
+	gluQuadricDrawStyle(quadObj, GLU_FILL);
+	gluQuadricNormals(quadObj, GLU_SMOOTH);
+	gluDisk(quadObj, 0.0, D, 32, 1);
+	gluDeleteQuadric(quadObj);
+
+	glPopMatrix();
+
+}
+void drawAxes(GLdouble length)
+{
+	glColor3f(1.0, 0.0, 0.0); // red x
+	glPushMatrix();
+	glTranslatef(-0.5f*length, 0, 0);
+	Arrow(0, 0, 0, 2 * length, 0, 0, 0.1);
+	glPopMatrix();
+
+	glColor3f(0.0, 1.0, 0.0); // green y
+	glPushMatrix();
+	glTranslatef(0, -0.5f*length, 0);
+	Arrow(0, 0, 0, 0, 2 * length, 0, 0.1);
+	glPopMatrix();
+
+	glColor3f(0.0, 0.0, 1.0); // blue z
+	glPushMatrix();
+	glTranslatef(0, 0, -0.5f*length);
+	Arrow(0, 0, 0, 0, 0, 2 * length, 0.1);
+	glPopMatrix();
 }
 
-void reshape(GLsizei width, GLsizei height) {  
-	if (height == 0) height = 1;                
-	GLfloat aspect = (GLfloat)width / (GLfloat)height;
 
-	glViewport(0, 0, width, height);
 
-	glMatrixMode(GL_PROJECTION);  
-	glLoadIdentity();             
-	gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+void moveInDir(float speed, float DirX, float DirY,float DirZ,float ObX,float ObY,float ObZ) {
+	ballPosX = ObX + speed * (DirX - ObX);
+	ballPosY = ObY + speed * (DirY - ObY);
+	ballPosZ = ObZ + speed * (DirZ - ObZ);
+	printf("%f %f %f %f %f %f %f \n", speed, DirX, DirY, DirZ, ballPosX, ballPosY, ballPosZ);
 }
 
 int main(int argc, char** argr)
 {
-	glutInit(&argc, argr);          
-	glutInitDisplayMode(GLUT_DOUBLE); 
-	glutInitWindowSize( window_width, window_height);
-	glutInitWindowPosition(50, 50); 
-	glutCreateWindow("Bouncing Ball");         
+	glutInit(&argc, argr);
+
+	glutInitWindowSize(window_width, window_height);
+	glutInitWindowPosition(50, 50);
+
+	glutCreateWindow("Bouncing Ball");
 	glutDisplayFunc(Display);
 	glutIdleFunc(Anim);
-	glutReshapeFunc(reshape);       
-	initGL();                      
-	glutMainLoop();                 
-	return 0;
+	glutSpecialFunc(key);
+	glutKeyboardUpFunc(spaceKeyUp);
+	glutKeyboardFunc(spaceKey);
+
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+
+	glEnable(GL_DEPTH_TEST);
+
+	
+	glutMainLoop();
 }
 
 void Display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glMatrixMode(GL_MODELVIEW);    
 
-	glLoadIdentity();                
-	glTranslatef(1.5f, 0.0f, -7.0f);
-	glRotatef(rotateCube, 1.0f, 1.0f, 1.0f);
-
-	glBegin(GL_QUADS);                
-	glColor3f(0.0f, 1.0f, 0.0f);     
-	glVertex3f(1.0f, 1.0f, -1.0f);
-	glVertex3f(-1.0f, 1.0f, -1.0f);
-	glVertex3f(-1.0f, 1.0f, 1.0f);
-	glVertex3f(1.0f, 1.0f, 1.0f);
-
-	glColor3f(1.0f, 0.5f, 0.0f);    
-	glVertex3f(1.0f, -1.0f, 1.0f);
-	glVertex3f(-1.0f, -1.0f, 1.0f);
-	glVertex3f(-1.0f, -1.0f, -1.0f);
-	glVertex3f(1.0f, -1.0f, -1.0f);
-
-	glColor3f(1.0f, 0.0f, 0.0f);   
-	glVertex3f(1.0f, 1.0f, 1.0f);
-	glVertex3f(-1.0f, 1.0f, 1.0f);
-	glVertex3f(-1.0f, -1.0f, 1.0f);
-	glVertex3f(1.0f, -1.0f, 1.0f);
-
-	glColor3f(1.0f, 1.0f, 0.0f);     
-	glVertex3f(1.0f, -1.0f, -1.0f);
-	glVertex3f(-1.0f, -1.0f, -1.0f);
-	glVertex3f(-1.0f, 1.0f, -1.0f);
-	glVertex3f(1.0f, 1.0f, -1.0f);
-
-	glColor3f(0.0f, 0.0f, 1.0f);    
-	glVertex3f(-1.0f, 1.0f, 1.0f);
-	glVertex3f(-1.0f, 1.0f, -1.0f);
-	glVertex3f(-1.0f, -1.0f, -1.0f);
-	glVertex3f(-1.0f, -1.0f, 1.0f);
-
-	glColor3f(1.0f, 0.0f, 1.0f);     
-	glVertex3f(1.0f, 1.0f, -1.0f);
-	glVertex3f(1.0f, 1.0f, 1.0f);
-	glVertex3f(1.0f, -1.0f, 1.0f);
-	glVertex3f(1.0f, -1.0f, -1.0f);
-	glEnd();  
-
-	glColor3f(1.0, 0.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glTranslatef(-1.5f, 0.0f, -7.0f);
-	glRotatef(rotateBall, 1.0f, 0.0f, 0.0f);
-	glutWireSphere(1.0, 20.0, 20.0);
+	gluPerspective(45.0f, window_width / window_height, 0.05f, 100);
 
-	glutSwapBuffers();
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0, 10, 15, 0.0f, 5, 0.0f, 0.0f, 1.0f, 0.0f);
+	glTranslatef(0.0f,0.0f,camera);
+	//glRotatef(camera, 0.0f, 0.0f,1.0f);
+
+	drawAxes(5);
+	
+	glPushMatrix();
+	//glRotatef(rotAng, 0, 1, 0);
+	
+	if (shoot == 1) {
+		moveByPower += Power;
+		moveInDir(moveByPower, PointerX, PointerY, -10,0,5,0);
+		glTranslatef(ballPosX, ballPosY, ballPosZ);
+		//printf("%f %f %f %f \n", Power, ballPosX, ballPosY, ballPosZ);
+	}
+	else {
+		glTranslatef(0, 5, 0);
+	}
+	glColor3f(0.0f, 1.0f, 1.0f);
+	glutSolidSphere(1,20,20);
+	glPopMatrix();
+	if(shoot==0)
+		Arrow(0.0f, 5, 0, PointerX, PointerY, -10, 0.1);
+
+	glColor3f(1.0, 0.0, 1.0);
+	glPushMatrix();
+	glScalef(10.0f, 10.0f, 1.0f);
+	glTranslatef(0.0f, 0.0f, -20.0f);
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	glFlush();
 }
 
 void Anim()
 {
-	if (rotateCube < 361.0f) rotateCube= rotateCube+0.1;
-	else rotateCube = 0.0f;
-	if (rotateBall < 361.0f) rotateBall = rotateBall +0.1;
-	else rotateBall = 0.0f;
+	rotAng += 0.01;
+
+	camera += add;
+	if ((int)camera == 0) add = 0.01;
+	if ((int)camera == 10) add = -0.01;
+
+	glutPostRedisplay();
+}	
+
+void key(int key, int x, int y)
+{
+	switch (key)
+	{
+	case GLUT_KEY_UP:
+		PointerY += 0.1;
+		break;
+	case GLUT_KEY_DOWN:
+		PointerY -= 0.1;
+		break;
+	case GLUT_KEY_LEFT:
+		PointerX -= 0.1;
+		break;
+	case GLUT_KEY_RIGHT:
+		PointerX += 0.1;
+		break;
+	}
+	glutPostRedisplay();
+
+}
+
+void spaceKey(unsigned char key, int x, int y) {
+	if (key == ' ')
+		Power += 0.002;
+	
+	//printf("%f", Power);
+	glutPostRedisplay();
+}
+
+void spaceKeyUp(unsigned char key, int x, int y) {
+	if (key == ' ')
+		shoot = 1;
 	glutPostRedisplay();
 }
